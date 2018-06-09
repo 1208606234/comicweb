505cf1f4f45b2673e90e685afc3451fcee9334d0
Raymond Kenny <1208606234@qq.com>
2018-06-09 13:21:18 +0800

    1.0
    登录界面

---
+++ 漫画狗/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>漫画狗--你想要的所有</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<div class="box">
+		<div class="top">
+
+			<br>
+			<p><img class="rect" src="images/toplogo.png"></p>
+			<br>
+			<br>
+			<p><img src="images/img_title.png">
+			
+			</p>
+		</div>
+		<div class="main">
+			<hr color="white">
+			
+			
+		</div>
+		<div class="bottom">
+		<hr color:white>
+		<p style="color:white">专注分享对世界的看法,和对acg文化的热爱</p>
+		
+		<hr color:white>
+			<p style="color:gray">版权所属@Raymond Kenny,是我做的.</p>
+		
+		
+		</div>
+	</div>
+</body>
+</html>
