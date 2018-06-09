46e72d35c4c98c6d4495919dd1a52c668c01a335
Raymond Kenny <1208606234@qq.com>
2018-06-09 15:46:09 +0800

    0.2

--- 漫画狗/index.php
+++ 漫画狗/index.php
@@ -2,36 +2,36 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>漫画狗--你想要的所有</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	<div class="box">
-		<div class="top">
-
-			<br>
-			<p><img class="rect" src="images/toplogo.png"></p>
-			<br>
-			<br>
-			<p><img src="images/img_title.png">
-			
-			</p>
+	<div class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<div class="navbar-brand"><a href="index.php" class="navbar-brand"></a></div>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="#">首页</a></li>
+				<li><a href="#">更新</a></li>
+				<li><a href="#">排行榜</a></li>
+				<li><a href="#">分类</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">登录</a></li>
+				<li><a href="#">注册</a></li>
+			</ul>
 		</div>
-		<div class="main">
-			<hr color="white">
-			
-			
+	</div>
+	<div class="container">
+		<div class="row">
+		<div class="col-sm-2">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ullam sequi dolores veritatis asperiores eius possimus inventore in modi a culpa, totam esse aperiam laudantium dolorem dignissimos, eos doloremque beatae.
 		</div>
-		<div class="bottom">
-		<hr color:white>
-		<p style="color:white">专注分享对世界的看法,和对acg文化的热爱</p>
-		
-		<hr color:white>
-			<p style="color:gray">版权所属@Raymond Kenny,是我做的.</p>
-		
-		
+		<div class="col-sm-7">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam maiores quidem rerum obcaecati recusandae deleniti dicta tempore optio, ut nam odio enim doloribus consequatur tempora, nihil reiciendis, porro ea nobis.</div>
+		<div class="col-sm-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id unde, ut placeat adipisci quod incidunt animi expedita enim similique sapiente? Consequuntur, accusamus dignissimos est, quod ad debitis cupiditate necessitatibus commodi!</div>
 		</div>
 	</div>
 </body>
---
+++ 漫画狗/sign.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>漫画狗--你想要的所有</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/sign.css">
+</head>
+<body>
+	<div class="box">
+		<div class="top">
+
+			<br>
+			<p><img class="rect" src="images/toplogo.png"></p>
+			<br>
+			<br>
+			<p><img src="images/img_title.png"></p>
+			<hr color="white">
+			<br>
+			<br>
+		</div>
+		<div class="container container-small">
+			<br>
+			<br>
+			<form action="" method="post">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="用户名/手机/邮箱">
+				</div>
+				<div class="form-group">
+					<input type="password" class="form-control" placeholder="密码">
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-primary btn-block">登录</button>
+				</div>
+			</form>
+			<div style="text-align:center"><h2 style="color:white;">没有账号?<a href="register.php">注册</a></h2></div>
+			
+			
+		</div>
+		<div class="bottom">
+		<hr color:white>
+		<p style="color:white">专注分享对世界的看法,和对acg文化的热爱</p>
+		
+		<hr color:white>
+			<p style="color:gray">版权所属@Raymond Kenny,是我做的.</p>
+		
+		
+		</div>
+	</div>
+</body>
+</html>
